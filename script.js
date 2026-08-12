0a0203e0537b1a6fd04ae255ae1821fa8bb5eb6b
bbuchler <bbuchler@github.com>
2026-08-12 11:52:05 -0700

    Add permanent schoolwork personalization survey

--- script.js
+++ script.js
@@ -56,7 +56,7 @@
 
     async function loadData() {
         try {
-            const response = await fetch('site-data.json?v=20260812-2');
+            const response = await fetch('site-data.json?v=20260812-3');
             siteData = await response.json();
             renderAll();
         } catch (err) {
